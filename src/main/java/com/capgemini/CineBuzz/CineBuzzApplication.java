0c0a41dbfe242ddc0f69029e0f478bf89515797c
Aditya Sinha <adityasinha28062004@gmail.com>
2025-05-13 14:57:57 +0530

    Initial Spring Boot project commit

---
+++ src/main/java/com/capgemini/CineBuzz/CineBuzzApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.CineBuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineBuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineBuzzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/CineBuzz/CineBuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.CineBuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
