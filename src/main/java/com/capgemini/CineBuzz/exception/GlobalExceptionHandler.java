8983a59cdfe9f9e819749e0d8726525d598715c3
Tanmxy88 <tanmaykadam75@gmail.com>
2025-05-14 10:59:15 +0530

    User entity First commit

---
+++ src/main/java/com/capgemini/CineBuzz/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.capgemini.CineBuzz.exception;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Object> handleUserNotFound(UserNotFoundException ex) {
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("timestamp", LocalDateTime.now());
+        errorDetails.put("message", ex.getMessage());
+        errorDetails.put("status", HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+  
+}
---
+++ src/main/java/com/capgemini/CineBuzz/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.capgemini.CineBuzz.repositories;
+
+import java.util.Optional;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.capgemini.CineBuzz.entities.User;
+
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	 Optional<User> findByEmail(String email);
+}
