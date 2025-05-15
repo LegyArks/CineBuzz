66658bc3901a33d8cf5ecbaa0df61b1ab7130caa
Rushikesh Thorve <rushikeshthorve@Rushikeshs-MacBook-Pro.local>
2025-05-15 09:38:07 +0530

    Predefinedtest

---
+++ src/main/java/com/capgemini/CineBuzz/exceptions/MethodNotAllowedException.java
@@ -0,0 +1,7 @@
+package com.capgemini.CineBuzz.exceptions;
+
+public class MethodNotAllowedException extends RuntimeException {
+    public MethodNotAllowedException(String message) {
+        super(message);
+    }
+}
